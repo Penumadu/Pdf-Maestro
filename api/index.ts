fbe283f36e1198b83cf3eb62fdc4da5b3cf468e3
Srinivasulu Penumadu <srinivasulu@Srinivasulus-MacBook-Pro.local>
2026-05-02 17:42:34 -0400

    Make serverless function fully standalone and add root dependencies

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,6 @@
 import express, { type Express } from "express";
 import cors from "cors";
 import { Router, type IRouter } from "express";
-import { HealthCheckResponse } from "@workspace/api-zod";
 
 const app: Express = express();
 
@@ -17,8 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 const router: IRouter = Router();
 
 router.get("/healthz", (_req, res) => {
-  const data = HealthCheckResponse.parse({ status: "ok" });
-  res.json(data);
+  res.json({ status: "ok" });
 });
 
 app.use("/api", router);
